c6f5fa0c26e8567ac4bf2c3f64b924b4ce2aa675
Bac Trach Dam <bac.dam.1991@gmail.com>
2021-11-07 12:23:33 +1100

    Can mutate user data.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 6969;
-const userData = require('./MOCK_DATA.json');
+let userData = require('./MOCK_DATA.json');
 const {
 	GraphQLSchema,
 	GraphQLObjectType,
@@ -51,7 +51,56 @@ const RootQuery = new GraphQLObjectType({
 	},
 });
 
-const schema = new GraphQLSchema({ query: RootQuery });
+const RootMutation = new GraphQLObjectType({
+	name: 'RootMutationType',
+	fields: {
+		createNewUser: {
+			type: UserType,
+			args: {
+				givenName: { type: GraphQLString },
+				familyName: { type: GraphQLString },
+				gender: { type: GraphQLString },
+				email: { type: GraphQLString },
+			},
+			resolve: function (parent, args) {
+				const newUser = { ...args, id: userData[userData.length - 1].id + 1 };
+				userData.push(newUser);
+				return newUser;
+			},
+		},
+		updateUserById: {
+			type: UserType,
+			args: {
+				id: { type: GraphQLInt },
+				givenName: { type: GraphQLString },
+				familyName: { type: GraphQLString },
+				gender: { type: GraphQLString },
+				email: { type: GraphQLString },
+			},
+			resolve: function (parent, args) {
+				const user = userData.find((user) => user.id === args.id);
+				const indexOf = userData.indexOf(user);
+				const updatedUser = { ...user, ...args };
+				userData[indexOf] = updatedUser;
+				return updatedUser;
+			},
+		},
+		deleteUserById: {
+			type: UserType,
+			args: {
+				id: { type: GraphQLInt },
+			},
+			resolve: function (parent, args) {
+				const deletedUser = userData.find((user) => user.id === args.id);
+				const filteredUsers = userData.filter((user) => user.id !== args.id);
+				userData = [...filteredUsers];
+				return deletedUser;
+			},
+		},
+	},
+});
+
+const schema = new GraphQLSchema({ query: RootQuery, mutation: RootMutation });
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
