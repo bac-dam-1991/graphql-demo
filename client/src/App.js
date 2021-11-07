7a5a981494d7ca60ba806b9a4efe7fb10dffc769
Bac Trach Dam <bac.dam.1991@gmail.com>
2021-11-08 07:30:00 +1100

    Setup apollo client.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	HttpLink,
+	from,
+} from '@apollo/client';
+
+const App = () => {
+	return <div>Hello</div>;
+};
+
+export default App;
